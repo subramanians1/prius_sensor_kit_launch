87d280ad17657105311aa6410c8165e74b2d2ef7
Saurabh Subramanian <s.subramanian1@tue.nl>
2025-10-16 14:17:27 +0200

    Created skeleton nebula container launch file and configured radar launch.

---
+++ common_sensor_launch/launch/nebula_radar_container.py
@@ -0,0 +1,104 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+import launch
+from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.actions import SetLaunchConfiguration
+from launch.conditions import IfCondition
+from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch_ros.parameter_descriptions import ParameterFile
+import yaml
+
+def launch_setup(context, *args, **kwargs):
+
+    # Configure Parameters
+
+    # Configure Composable Nodes
+
+    nodes = []
+
+    nodes.append(
+        ComposableNode(
+            package="glog_component",
+            plugin="GlogComponent",
+            name="glog_component"
+        )
+    )
+
+    nodes.append(
+        ComposableNode(
+            package="nebula_ros",
+            plugin=sensor_make + "RosWrapper",
+            name=sensor_make.lower() + "_ros_wrapper_node",
+            parameters=[
+                "host_ip",
+                "sensor_ip",
+                "data_port",
+                "frame_id",
+                "base_frame",
+                "object_frame",
+                "launch_hw",
+                "multicast_ip",
+                "sensor_model",
+                "configuration_host_port",
+                "configuration_sensor_port",
+                "use_sensor_time",
+                "configuration_vehicle_length",
+                "configuration_vehicle_width",
+                "configuration_vehicle_height",
+                "configuration_vehicle_wheelbase"
+            ],
+            remappings=[]
+        )
+    )
+
+def generate_launch_description():
+    launch_arguments = []
+
+    def add_launch_arg(name: str, default_value=None, description=None):
+        # a default_value of None is equivalent to not passing that kwarg at all
+        launch_arguments.append(
+            DeclareLaunchArgument(name, default_value=default_value, description=description)
+        )
+    
+    common_sensor_share_dir = get_package_share_directory("common_sensor_launch")
+
+    add_launch_arg("host_ip", "192.16.2.200", "host ip address")
+    add_launch_arg("sensor_ip", "192.168.2.113", "sensor ip address")
+    add_launch_arg("data_port", "42102", "port at which data stream from sensor arrives")
+    add_launch_arg("frame_id", "radar", "tf2 frame name for published sensor data")
+    add_launch_arg("base_frame", "base_link", "base frame id")
+    add_launch_arg("object_frame", "base_link", "tracked object frame id")
+    add_launch_arg("launch_hw", "true", "whether to connect to real sensor")
+    add_launch_arg("multicast_ip", "224.0.2.2", "multicast ip address")
+    add_launch_arg("sensor_model", "ARS548", "sensor model. can be ARS548 or SRR520")
+    add_launch_arg("configuration_host_port", "42402", "host port")
+    add_launch_arg("configuration_sensor_port", "42102", "port at which data stream from sensor arrives")
+    add_launch_arg("use_sensor_time", "false", "use sensor time for published data")
+    add_launch_arg("configuration_vehicle_length", "4.645", "vehicle length")
+    add_launch_arg("configuration_vehicle_width", "1.77", "vehicle width")
+    add_launch_arg("configuration_vehicle_height", "1.49", "vehicle height")
+    add_launch_arg("configuration_vehicle_wheelbase", "2.70", "vehicle wheelbase")
+    add_launch_arg("use_multithread", "false", "use multithread")
+
+    set_container_executable = SetLaunchConfiguration(
+        "container_executable",
+        "component_container",
+        condition=UnlessCondition(LaunchConfiguration("use_multithread")),
+    )
+
+    set_container_mt_executable = SetLaunchConfiguration(
+        "container_executable",
+        "component_container_mt",
+        condition=IfCondition(LaunchConfiguration("use_multithread")),
+    )
+
+    return launch.LaunchDescription(
+        launch_arguments
+        + [set_container_executable, set_container_mt_executable]
+        + [OpaqueFunction(function=launch_setup)]
+    )
